854962e2794cc7672e6698eb98410fa8fdf17179
fulmoon <fulmoon@gmail.com>
2022-08-12 08:53:19 +0900

    Refact: add viewmodel for provider    #Air

--- lib/counter_view_model.dart
+++ lib/counter_view_model.dart
@@ -1,3 +1,15 @@
-class CounterViewModel{
+import 'package:flutter/material.dart';
 
+class CounterViewModel extends ChangeNotifier{
+  int count = 0;
+  bool isLoading = false;
+
+  void increment(){
+    isLoading = true;
+    notifyListeners();
+
+    count++;
+    isLoading = false;
+    notifyListeners();
+  }
 }
